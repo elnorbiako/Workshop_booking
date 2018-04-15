e40f15cf40dcb6b246809727eb762edee7dfa4e7
Norbert Różyc <elnorbiako@gmail.com>
2018-04-15 21:26:41 +0200

    User menu for adding reservations

--- src/pl/coderslab/entity/Main6.java
+++ src/pl/coderslab/entity/Main6.java
@@ -8,10 +8,64 @@ public class Main6{
 	
 	public static void main(String[] args) {
 
-		
-		Reservation reserv = new Reservation("2018-05-05", "2018-05-13", "Nie będzie niczego",1,4,3 );
-		Reservation.add(reserv);
-		
+		userMenuReserv();		
 		
 	}
+	/**
+	 * User menu for adding reservations (with user id, room id, date from and to, description)
+	 */
+	public static void userMenuReserv() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Room list: ");
+		System.out.println(ArrayUtils.toString(Room.loadAllRooms()));	
+		
+		System.out.println("Choose option: 'add' to create new reservation, or 'quit' to end :");
+		String command = scan.nextLine();
+		
+		while(!command.equals("quit")) {
+			
+			try {			
+										
+				if (command.equals("add")) {
+					
+					System.out.println("User id:");
+					int userId = scan.nextInt();
+					scan.nextLine();
+					
+					System.out.println("Room id:");
+					int roomId = scan.nextInt();
+					scan.nextLine();
+					
+					System.out.println("Start date (yyyy-mm-dd):");
+					String dateFrom = scan.nextLine();
+					
+					System.out.println("End date (yyyy-mm-dd):");
+					String dateTo = scan.nextLine();
+					
+					System.out.println("Additional informations:");
+					String description = scan.nextLine();
+					
+					Reservation reserv = new Reservation(dateFrom, dateTo, description, 1, userId, roomId);
+					
+					Reservation.add(reserv);
+					
+				} else {
+					System.out.println("Please try again.");
+					}
+				
+			} catch (NumberFormatException e) {
+				System.out.println("Not a number!");
+			}
+								
+			System.out.println("Reservation list: ");
+			System.out.println(ArrayUtils.toString(Reservation.loadAllReservations()));	
+			System.out.println("Choose option: 'add' to create new reservation, or 'quit' to end :");
+			command = scan.nextLine();
+				}
+		scan.close();
+		}
 }
+
+					
